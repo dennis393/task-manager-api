857c11b486e866bbbe5cf7b1c4a86213c52a94d7
dennis393 <jorikms30@gamil.com>
2026-08-13 16:58:59 +0500

    Добавил фикстуры тестов и ReadMe

--- tests.py
+++ tests.py
@@ -1,6 +1,49 @@
 from fastapi.testclient import TestClient
 from routs import fake_db
 from main import app
+import pytest
+
+
+#Фикстуры
+#Сама фикстура, которую в дальнейшем нужно предавать в качестве параметра функции
+@pytest.fixture
+def client(): #Главная фикстура, которая возвращает подключение к API
+    return TestClient(app)
+
+@pytest.fixture
+def reset_db(autouse=True): #Фикстура которая очищает базу и приводит ее в исходное состояние
+    fake_db.clear() #Очищаем базу
+    fake_db.update({
+        1: {"task_id": 1, "task_name": "Изучить FastAPI", "task_status": False},
+        2: {"task_id": 2, "task_name": "Порешать задачи на LeetCode", "task_status": True}
+    }) #Приводим базу в первоначальное состояние
+    
+def test_get_list_tasks(client): #Тестирование метода GET
+
+    responce = client.get("/Get_tasks") #Отправляем запрос get к эндпоинту который отвечает за возврат задач
+    assert responce.status_code == 200 #Проверка, что сервер вернул статус 200
+    assert responce.json() == {
+    "1": {"task_id": 1, "task_name": "Изучить FastAPI", "task_status": False},
+    "2": {"task_id": 2, "task_name": "Порешать задачи на LeetCode", "task_status": True}
+} #Проверяем что сервер вернул JSON точно такого же формата как написан у меня
+
+def test_create_new_task(client): #Тестирование метода Post
+    responce = client.post("/Create_task?task_name=Новая задача&task_status=false") #Передаем Query парметры напрямую, иначе
+    #Имитация запроса на создание новой задачи
+    assert responce.status_code == 200 #проверка на успешный статус код
+    assert responce.json()["task_name"] == "Новая задача" #Проверяем что поле task_name совпадает с тем, что отправили
+    assert responce.json()["task_status"] == False
+    
+def test_put_status_task(client): #Тестирование метода Put
+    responce = client.put("/Tasks/1?task_status=true") #Шлем запрос на обновления статуса
+    assert responce.status_code == 200 #Проверяем что сервер возвращаеи 200 статус
+    assert responce.json()["task_status"] == True #Проверяем что статус действительно обновился
+
+def test_delete_task(client): #Тестирование метода Delete
+    responce = client.delete("/Delete_task/1")
+    assert responce.status_code == 200 #Проверяем что сервер возвращаеи 200 статус
+    assert responce.json() == {"msg": "Задача успешно удалена"}
+"""
 
 client = TestClient(app)
 
@@ -33,4 +76,6 @@ def test_delete_task(): #Тестирование метода Delete
 def test_delete_task_404_status():#Тестирование метода Delete, на статус код 404
     responce = client.delete("/Delete_task/10")
     assert responce.status_code == 404 #Проверяем что сервер возвращает 404 статус
+"""
+
     
